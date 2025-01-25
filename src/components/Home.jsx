a4c79726d1cd3020e05228d1188518c7f693f306
Mohd-Affan-Code <mohdaffanjkhan@gamil.com>
2025-01-25 10:49:08 +0530

    home transol

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Linkedin, Github } from "lucide-react";
 import { skillPng } from "../assets/skill.js";
 import india from "/skillImg/india.png";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+  const { t } = useTranslation();
   // Divide the skillPng array into pairs of 2
   const groupedSkills = [];
   for (let i = 0; i < skillPng.length; i += 2) {
@@ -14,7 +16,7 @@ const Home = () => {
     <div id="hero" className="bg-homeColer min-h-[calc(100vh-96px)] py-10">
       <div className="mx-auto max-w-[79rem] px-4 mt-20 flex flex-col justify-center gap-6">
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-800 ">
-          Front-End Web Developer
+          {t("home.title")}
         </h1>
         <p className="font-medium text-xl text-gray-700 my-4 leading-10">
           Hi, I'm <span className="underline"> Mohd AFFAN.</span> An expert
@@ -49,14 +51,14 @@ const Home = () => {
             download="MohdAffan.pdf"
             className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
           >
-            Download Resume
+            {t("home.downloadResume")}
           </a>
         </div>
 
         <div className="flex max-md:flex-col md:items-center items-start gap-8 mt-14">
           {/* My Skills Heading */}
           <span className="font-semibold text-lg whitespace-nowrap">
-            My Skills:
+            {t("home.skillsHeading")}:
           </span>
 
           {/* Skills Images Wrapper */}
